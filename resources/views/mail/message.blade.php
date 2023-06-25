44c724ff00a83f0a30ffaeabcdf76f5f1ee79a8b
João F. Ramos <joao.ramos1988@outlook.com>
2023-06-25 12:45:26 -0300

    Correção tag p telefone

--- resources/views/mail/message.blade.php
+++ resources/views/mail/message.blade.php
@@ -1,5 +1,6 @@
 <h2>Você recebeu uma nova mensagem</h2>
 <p><strong>Nome:</strong> {{ $data[0]['name'] }}</p>
-<p><strong>Nome:</strong> {{ $data[0]['tel'] }}</p>
+<p><strong>Telefone:</strong> {{ $data[0]['tel'] }}</p>
 <p><strong>E-mail:</strong> {{ $data[0]['email'] }}</p>
-<p><strong>Mensagem:</strong> {{ $data[0]['message'] }}</p>
+<p><strong>Mensagem:</strong></p>
+<p>{{ $data[0]['message'] }}</p>
