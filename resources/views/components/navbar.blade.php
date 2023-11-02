cf8638222ed7ae1ad4e5b241c063dd82fb8fec0a
João F. Ramos <joao.ramos1988@outlook.com>
2023-11-02 17:38:13 -0300

    Adição de componets navbar e footer

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,54 @@
+<div class="container-fluid bar">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+            <button class="navbar-toggler bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <strong><a class="nav-link" href="#experience">Experience | Education</a></strong>
+                    </li>
+                    <li class="nav-item">
+                        <strong><a class="nav-link" href="#skills">Skills | Awards</a></strong>
+                    </li>
+                    <li class="nav-item">
+                        <strong><a class="nav-link" href="#projects">Projects</a></strong>
+                    </li>
+                    <li class="nav-item">
+                        <strong><a class="nav-link" href="#interests">Interests</a></strong>
+                    </li>
+                    <li class="nav-item">
+                        <strong><a class="nav-link" href="#contact">Contact</a></strong>
+                    </li>
+                </ul>
+            </div>
+            <ul class="nav navbar float-end social-icons">
+                <li class="nav-item me-2">
+                    <a class="social-icon" href="https://linkedin.com/in/joao-f-ramos1104" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                </li>
+                <li class="nav-item me-2">
+                    <a class="social-icon" href="https://github.com/joaoramos1104" target="_blank"><i class="fab fa-github"></i></a>
+                </li>
+                <li class="nav-item me-2">
+                    <a class="social-icon" href="https://www.instagram.com/f.ramosjoao" target="_blank"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li class="nav-item me-2">
+                    {{-- <a class="social-icon" href="https://twitter.com/joaoramos1988" target="_blank"><i class="fab fa-twitter"></i></a> --}}
+                    <a class="social-icon" href="https://twitter.com/joaoramos1988" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+                        <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z"/>
+                      </svg></a>
+                </li>
+            </ul>
+            <ul class="nav navbar float-end social-icons">
+                <li class="nav-item me-2">
+                    <!--<a class="nav-link" href="{{ route('admin') }}" target="_blank"><i class="fas fa-user"></i> Login</a>-->
+                    <button class="switch" id="switch">
+                        <span><i class="fas fa-sun me-2"></i></span>
+                        <span><i class="fas fa-moon ms-2"></i></span>
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
--- storage/framework/views/2446c049ea960e333c0a6286a9123e5df261daad.php
+++ storage/framework/views/2446c049ea960e333c0a6286a9123e5df261daad.php
@@ -2,63 +2,20 @@
 
     <!-- header -->
     <header>
-        <div class="container-fluid bar">
-            <nav class="navbar navbar-expand-lg navbar-dark">
-                <div class="container">
-                    <button class="navbar-toggler bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <strong><a class="nav-link" href="#experience">Experience | Education</a></strong>
-                            </li>
-                            <li class="nav-item">
-                                <strong><a class="nav-link" href="#skills">Skills | Awards</a></strong>
-                            </li>
-                            <li class="nav-item">
-                                <strong><a class="nav-link" href="#projects">Projects</a></strong>
-                            </li>
-                            <li class="nav-item">
-                                <strong><a class="nav-link" href="#interests">Interests</a></strong>
-                            </li>
-                            <li class="nav-item">
-                                <strong><a class="nav-link" href="#contact">Contact</a></strong>
-                            </li>
-                        </ul>
-                    </div>
-                    <ul class="nav navbar float-end social-icons">
-                        <li class="nav-item me-2">
-                            <a class="social-icon" href="https://linkedin.com/in/joao-f-ramos1104" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-                        </li>
-                        <li class="nav-item me-2">
-                            <a class="social-icon" href="https://github.com/joaoramos1104" target="_blank"><i class="fab fa-github"></i></a>
-                        </li>
-                        <li class="nav-item me-2">
-                            <a class="social-icon" href="https://www.instagram.com/f.ramosjoao" target="_blank"><i class="fab fa-instagram"></i></a>
-                        </li>
-                        <li class="nav-item me-2">
-                            
-                            <a class="social-icon" href="https://twitter.com/joaoramos1988" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
-                                <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z"/>
-                              </svg></a>
-                        </li>
-                    </ul>
-                    <ul class="nav navbar float-end social-icons">
-                        <li class="nav-item me-2">
-                            <!--<a class="nav-link" href="<?php echo e(route('admin')); ?>" target="_blank"><i class="fas fa-user"></i> Login</a>-->
-                            <button class="switch" id="switch">
-                                <span><i class="fas fa-sun me-2"></i></span>
-                                <span><i class="fas fa-moon ms-2"></i></span>
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-        <div class="header">
+       <?php if (isset($component)) { $__componentOriginal08d9d46900ea68d5dc06d8728734a2fd47ca153c = $component; } ?>
+<?php $component = $__env->getContainer()->make(App\View\Components\Navbar::class, []); ?>
+<?php $component->withName('navbar'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes([]); ?>
+<?php if (isset($__componentOriginal08d9d46900ea68d5dc06d8728734a2fd47ca153c)): ?>
+<?php $component = $__componentOriginal08d9d46900ea68d5dc06d8728734a2fd47ca153c; ?>
+<?php unset($__componentOriginal08d9d46900ea68d5dc06d8728734a2fd47ca153c); ?>
+<?php endif; ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+        <div class="header"></div>
 
-        </div>
         <div class="container">
             <?php $__currentLoopData = $profiles; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $profile): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                 <div class="row">
