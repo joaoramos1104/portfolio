f48157a1ecb7766c3fc8a59a0d84359e7990f5e8
João F. Ramos <joao.ramos1988@outlook.com>
2023-11-02 16:55:17 -0300

    Atualização do icone do Twitter x

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -39,7 +39,10 @@
                             <a class="social-icon" href="https://www.instagram.com/f.ramosjoao" target="_blank"><i class="fab fa-instagram"></i></a>
                         </li>
                         <li class="nav-item me-2">
-                            <a class="social-icon" href="https://twitter.com/joaoramos1988" target="_blank"><i class="fab fa-twitter"></i></a>
+                            {{-- <a class="social-icon" href="https://twitter.com/joaoramos1988" target="_blank"><i class="fab fa-twitter"></i></a> --}}
+                            <a class="social-icon" href="https://twitter.com/joaoramos1988" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+                                <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z"/>
+                              </svg></a>
                         </li>
                     </ul>
                     <ul class="nav navbar float-end social-icons">
